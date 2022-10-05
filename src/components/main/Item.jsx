3a06ef61001f1b919e5d7445650583346a25a121
sofivasconi <sofivasconi@gmail.com>
2022-10-05 13:55:46 -0300

    segunda entrega

---
+++ src/components/main/Item.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+/*
+const Item = () => {
+    return (
+        <section class="item">
+            <div class="imgItem">
+                <img src="${camiseta.imagen}" alt="${camiseta.titulo}" class="imagen">
+            </div>
+            <div class="titulo">
+                <h3 class="h3Titulo"><strong >${camiseta.titulo}</strong></h3>
+                <h3 class="h3Precio"><small>$ ${camiseta.precio}</small></h3>
+            </div>
+            <div class="agregarAlCarrito" onclick="agregarAlCarrito(${camiseta.id})">
+                <i class='bx bx-cart-add' id="itemCarrito" />
+            </div>
+        </section>
+    );
+};
+*/
+
+export default Item; 
